be5fd1ee0f9845f328a6a1d355aaa7e80335918f
joshua1988 <jangkeehyo@gmail.com>
2017-07-25 21:20:25 +0900

    Updated the simple vuex tutorial along with vue.js pdf file

---
+++ samples/vuex-sample/captain-pangyo/simple-vuex/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    Parent counter : {{ this.$store.state.counter }} <br>
+    <button @click="addCounter">+</button>
+    <button @click="subCounter">-</button>
+
+    <child></child>
+  </div>
+</template>
+
+<script>
+import Child from './Child.vue'
+
+export default {
+  data () {
+    return {
+      // counter: this.$store.state.counter
+    }
+  },
+  methods: {
+    addCounter() {
+      this.$store.state.counter++;
+    },
+    subCounter() {
+      this.$store.state.counter--;
+    }
+  },
+  components: {
+    'child': Child
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ samples/vuex-sample/captain-pangyo/simple-vuex/src/Child.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <hr>
+    Child counter : {{ this.$store.state.counter }} <br>
+    <button>+</button>
+    <button>-</button>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+</style>
